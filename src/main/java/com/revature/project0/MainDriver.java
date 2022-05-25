b219f3d27e65f83a28bb55eb37abe3c53a84986a
ML <labrecquemegan@hotmail.com>
2022-05-25 15:14:26 -0400

    added userDAO to main driver

--- src/main/java/com/revature/project0/MainDriver.java
+++ src/main/java/com/revature/project0/MainDriver.java
@@ -3,15 +3,14 @@ package com.revature.project0;
 //allows us to use files in different packages
 import com.revature.project0.services.UserService;
 import com.revature.project0.ui.StartMenu;
+import com.revature.project0.daos.UserDAO;
 
 //This Starts our application and helps us navigate
 
 public class MainDriver {
     public static void main(String[] args) {
 //        creates a new userService model so that it is accessible in this file and in subsequent methods
-        UserService userService = new UserService();
-
 //        anonymous function: function will disappear after the start method executes
-        new StartMenu(userService).start();
+        new StartMenu(new UserService(new UserDAO())).start();
     }
 }
